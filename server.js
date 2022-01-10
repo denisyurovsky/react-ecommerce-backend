ab6e71e45b66ada0554b91ecea1aae1c5428821b
AndreiKachur <underfil@mail.ru>
2022-01-10 11:06:47 +0300

    Fix: connect json-server-auth correctly

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const data = require('./data');
 const constants = require('./constants.js');
 
 const jsonServer = require('json-server');
+const auth = require('json-server-auth');
 const server = jsonServer.create();
 const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
+
+server.db = router.db;
+
+const rules = auth.rewriter({
+  "/comments*": "/644/comments$1",
+  "/products*": "/644/products$1",
+  "/users*": "/644/users$1",
+});
 
 server.use(
   middlewares,
@@ -24,6 +33,8 @@ server.patch('/products/:id', (req, res, next) => {
   next();
 });
 
+server.use(rules);
+server.use(auth);
 server.use(router);
 
 server.listen(constants.PORT_URL, () => {
