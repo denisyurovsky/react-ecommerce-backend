e31b5ba66f150b4aa98622a480953cc00656251c
Vasiliy Kutuzov <Vasiliy_Kutuzov@epam.com>
2022-01-26 13:54:39 +0300

    Modify cart endpoint due to changes on frontend

--- src/validators/checkCartValidity.js
+++ src/validators/checkCartValidity.js
@@ -1,11 +1,5 @@
 module.exports = (cart) => {
-  if (
-    !Array.isArray(cart.products) ||
-    !Number.isInteger(cart.totalQuantity) ||
-    !Number.isInteger(cart.totalPrice)
-  ) {
-    return false;
-  }
-
-  return true;
+  return (
+    Number.isInteger(cart.totalQuantity) && Number.isInteger(cart.totalPrice)
+  );
 };
