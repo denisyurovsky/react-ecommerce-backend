815da32a625c5ba93bb478962e5c5d53a10ddb59
Vasiliy <80971615+Olkerty@users.noreply.github.com>
2022-02-01 13:11:19 +0300

    Add discount price field to products

--- src/data/products.js
+++ src/data/products.js
@@ -4,9 +4,9 @@ const _ = require('lodash');
 const { users } = require('./users');
 const { categories } = require('./categories');
 const {
-    NUMBER_OF_PRODUCTS,
-    USER_ROLE,
-    TEST_IMAGE_URLS,
+  NUMBER_OF_PRODUCTS,
+  USER_ROLE,
+  TEST_IMAGE_URLS,
 } = require('../constants.js');
 
 faker.seed(1);
@@ -15,24 +15,27 @@ const products = [];
 const sellers = _.filter(users, ['role', USER_ROLE.SELLER]);
 
 for (let i = 0; i < NUMBER_OF_PRODUCTS; i++) {
-    const createdAt = faker.date.past();
-    const updatedAt = faker.date.between(createdAt, new Date());
+  const createdAt = faker.date.past();
+  const updatedAt = faker.date.between(createdAt, new Date());
 
-    products.push({
-        id: i,
-        userId: faker.random.arrayElement(sellers).id,
-        name: faker.commerce.productName(),
-        price: Number(faker.commerce.price()),
-        description: faker.commerce.productDescription(),
-        category: {
-            id: faker.datatype.uuid(),
-            name: faker.random.arrayElement(categories),
-        },
-        rating: null,
-        images: TEST_IMAGE_URLS[i] ? TEST_IMAGE_URLS[i] : [],
-        createdAt,
-        updatedAt
-    });
-};
+  const discountPrice = Number(faker.commerce.price());
+  const price = Number(faker.commerce.price());
+  products.push({
+    id: i,
+    userId: faker.random.arrayElement(sellers).id,
+    name: faker.commerce.productName(),
+    price: price,
+    discountPrice: discountPrice < price ? discountPrice : null,
+    description: faker.commerce.productDescription(),
+    category: {
+      id: faker.datatype.uuid(),
+      name: faker.random.arrayElement(categories),
+    },
+    rating: null,
+    images: TEST_IMAGE_URLS[i] ? TEST_IMAGE_URLS[i] : [],
+    createdAt,
+    updatedAt,
+  });
+}
 
 module.exports = { products };
