43dbf74fc7b08a89bf57726a05e36ab04291c8d1
Denis Iurovskii <denis_iurovskii@epam.com>
2022-02-17 14:32:10 +0300

    Remove field password from the endpoints /users, /users/<ID>

--- src/middlewares/response.js
+++ src/middlewares/response.js
@@ -1,21 +1,30 @@
 const checkCurrentUser = require('../helpers/checkCurrentUser');
 const getBaseUrl = require('../helpers/getBaseUrl');
 
+const modifyAllUsersResponse = (body, req) => {
+  body
+    .map((user) => delete user.password)
+    .filter((user) => !checkCurrentUser(user, req))
+    .map((user) => delete user.cart);
+};
+
+const modifySingleUserResponse = (body, req) => {
+  delete body.password;
+  if (!checkCurrentUser(body, req)) {
+    delete body.cart;
+  }
+};
+
 module.exports = (req, res) => {
   const body = res.locals.data;
   const baseUrl = getBaseUrl(req.url);
-  const checkConfidentialUser = (user) => !checkCurrentUser(user, req);
 
   switch (baseUrl) {
     case 'users':
       if (Array.isArray(body)) {
-        body
-          .filter((user) => checkConfidentialUser(user))
-          .forEach((user) => delete user.cart);
-      } else if (checkConfidentialUser(body)) {
-        delete body.cart;
+        modifyAllUsersResponse(body, req);
       }
+      modifySingleUserResponse(body, req);
   }
-
   res.json(body);
 };
