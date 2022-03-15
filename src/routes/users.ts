60962b4248afceaa41153f2fa05c80c13a23a632
Dmitrii Maletskii <dmitrii_maletskii@epam.com>
2022-03-16 00:21:12 +0300

    Fix: User avatar on Header is NOT updated after changes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,16 +1,17 @@
 import { writeFileSync } from 'fs';
 
 import type { Request, Response, NextFunction } from 'express';
+import _ from 'lodash';
 
 import { ORIGIN_URL } from '../constants';
 import removeImages from '../helpers/removeImages';
 
 export default (req: Request, res: Response, next: NextFunction): void => {
   if (req.body.avatar?.indexOf('base64') >= 0) {
-    const { id, avatar } = req.body;
+    const { avatar } = req.body;
     const [fileParameters, base64img] = avatar.split(';base64,');
     const extension = fileParameters.split('/')[1];
-    const publicFolderPath = `users/images/profile${id}.${extension}`;
+    const publicFolderPath = `users/images/${_.uniqueId('avatar')}.${extension}`;
     const relativePath = `public/${publicFolderPath}`;
 
     writeFileSync(relativePath, base64img, { encoding: 'base64' });
