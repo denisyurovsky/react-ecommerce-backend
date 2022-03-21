30845937a2701e64812f8a76699dcce727fd8071
Dmitrii Maletskii <mltsk94@gmail.com>
2022-03-22 02:53:15 +0300

    Fix: After the order is created by the guest, the "Page not found" appears

--- src/helpers/extractUserIdFromRequest.ts
+++ src/helpers/extractUserIdFromRequest.ts
@@ -1,10 +1,10 @@
 import type { Request } from 'express';
 import jwtDecode, { JwtPayload } from 'jwt-decode';
 
-export default function extractUserIdFromRequest(req: Request): number | undefined {
+export default function extractUserIdFromRequest(req: Request): number | null {
   const token = req.get('Authorization')?.replace(/Bearer\s/, '');
 
-  if (!token) return;
+  if (!token) return null;
 
   const decoded = jwtDecode<JwtPayload>(token);
 
